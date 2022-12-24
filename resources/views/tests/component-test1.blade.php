4faf35508a89094f0b9f2ce7ccacb32e1f9498a1
taiseih <taisei.hayashizaki@gmail.com>
2022-12-24 17:12:12 +0900

    コミット忘れ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -6,7 +6,7 @@
 コンポーネントテスト１
 </x-tests.app>
 
-<x-tests.card title="属性に" content="指定して属性を指定してる" :message="$message">
+<x-tests.card title="属性" content="を指定してる" :message="$message">
     
         $slotで表示してる
    組み合わせて使用することができる
